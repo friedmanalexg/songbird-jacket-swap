4168df426731464b1b3cadd276eb4f30ff118357
Alex <friedmanalexg@gmail.com>
2023-03-30 14:03:17 -0400

    hello world posts

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+print("Hello World!")
+
+
 @app.route('/')
 def index():
-    return 'credibleDEV Test'
+    return 'hello world!'
+
 
 if __name__ == '__main__':
     app.run()
